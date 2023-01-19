1c653bffc55850b14e2eba0b93c1497792bbd988
Melvin Kusters <melvin.kusters@quotationfactory.com>
2023-01-19 08:45:17 +0100

    Fixed SensorDataMonitor usage.

--- EB.Sensors/EB.Sensors.SignalR/Features/SensorData/Hubs/SensorHub.cs
+++ EB.Sensors/EB.Sensors.SignalR/Features/SensorData/Hubs/SensorHub.cs
@@ -9,7 +9,6 @@ public class SensorHub : Hub
 {
     private readonly ILogger<SensorHub> _logger;
     private readonly SensorDataHandler _sensorDataProvider;
-    private readonly SensorDataMonitor _sensorDataMonitor;
     
     public event SensorDataDelegates.ReceiveSensorDataDelegate ReceiveSensorDataEvent;
 
@@ -21,15 +20,12 @@ public class SensorHub : Hub
         
         _sensorDataProvider = new SensorDataHandler(
             factory.CreateLogger<SensorDataHandler>());
-        
-        _sensorDataMonitor = new SensorDataMonitor(
-            factory.CreateLogger<SensorDataMonitor>(), 
-            "EcodorpBoekelPrimary", 
-            ReceiveSensorDataEvent);
     }
 
     public async Task ReceiveSensorData(Infrastructure.Models.SensorData data)
     {
+        _sensorDataProvider.AddData(data);
+
         await Clients.All.SendAsync("ReceiveSensorData", data.SensorId, data.Value, data.Created);
     }
 }
